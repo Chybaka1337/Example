f4679fa3bd9232c5343d288f9496ba0133a05aeb
Andrey Zommer <zommer.03@gmail.com>
2023-07-13 13:22:36 +0300

    add Example02

---
+++ Example02/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("intput your name: ");
+string userName = Console.ReadLine();
+Console.WriteLine($"Hello, {userName}!");
